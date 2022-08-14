a36cf20779e9a03069d015fd6b1100021c3ede8e
Emre Arslan <emre@lenasoftware.com>
2022-08-14 18:30:14 +0300

    other project create

---
+++ EA.FoodRecipe.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EA.FoodRecipe.Business
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
